39e7c61f9314e3a8c4049145580eb9a9a566f748
Pablet <pablosaavedra6@gmail.com>
2024-12-30 23:31:00 -0300

    .

--- 1/sql_query_1.sql
+++ 1/sql_query_1.sql
@@ -6,7 +6,8 @@ SELECT
     e.oh_p50 as OH
 FROM galSpecInfo AS i
 JOIN galSpecExtra AS e ON i.specObjID = e.specObjID
-WHERE i.z > 0.005 AND i.z < 0.25
+WHERE i.z > 0.005 AND i.z < 0.3
+    AND e.sfr_tot_p50 > -2 AND e.sfr_tot_p50 < 2
     AND e.lgm_tot_p50 != -9999
     AND e.sfr_tot_p50 != -9999
     AND e.oh_p50 != -9999
